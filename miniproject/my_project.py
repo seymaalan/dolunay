a7398df4dc5c0ed8496ce360f21aba73e203fd3c
gulsumsaluvan <gulsumsaluvan5@gmail.com>
2024-02-09 22:38:55 +0300

    9.02.2024

--- miniproject/my_project.py
+++ miniproject/my_project.py
@@ -4,3 +4,9 @@ r = requests.get("http://api.open-notify.org/astros.json")
 
 print(r.status_code)
 print(r.json())
+import json
+def jprint(obj):
+    #create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+jprint(r.json())
