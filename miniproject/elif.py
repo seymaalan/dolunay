d50f568890ed455459094ff61289f4207b88e1a5
elifkirer <elifkirer1@gmail.com>
2024-02-13 01:09:25 +0300

    2.c

--- miniproject/elif.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-response = requests.get("https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=g6pKevuLGDIhUR5eudnnOiWtvpW6SeDf")
-def jprint(obj):
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-jprint(response.json())  
-def fetch_events():
-      
