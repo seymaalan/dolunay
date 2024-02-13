d1861d85f2e2f1114f55bee64ae5efeeab151307
elifkirer <elifkirer1@gmail.com>
2024-02-14 01:53:02 +0300

    csv done

--- miniproject/elif.py
+++ miniproject/elif.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import csv
 
 url = "https://app.ticketmaster.com/discovery/v2/events.json?size=20&countryCode=TR&dmaID=613&apikey=g6pKevuLGDIhUR5eudnnOiWtvpW6SeDf"
 
@@ -14,61 +15,51 @@ def fetch_events():
      response = requests.get(url, parameters)
 
      if response.status_code==200:
-         # jprint(response.json())
          x = 0
          while x<20:
              
              data = response.json()
              try:
               event_name = data["_embedded"]["events"][x]["name"]
-              
              except KeyError:
                  event_name = "None" 
 
              try:
               genre_name = data["_embedded"]["events"][x]["classifications"][0]["genre"]["name"]
-            
              except KeyError:
                 genre_name = "None" 
 
              try:
               segment_name = data["_embedded"]["events"][x]["classifications"][0]["segment"]["name"]
-              
              except KeyError:
                 segment_name = "None" 
 
              try:
               address = data["_embedded"]["events"][x]["_embedded"]["venues"][0]["address"]["line1"]
-              
              except KeyError:
                 address = "None"
 
              try:
               city_name = data["_embedded"]["events"][x]["_embedded"]["venues"][0]["city"]["name"]
-              
              except KeyError:
                 city_name = "None"
 
              try:
               localdate = data["_embedded"]["events"][x]["dates"]["start"]["localDate"]
-             
              except KeyError:
                 localdate = "None"
 
              try:
               localtime = data["_embedded"]["events"][x]["dates"]["start"]["localTime"]
-              
              except KeyError:
                 localtime = "None"
 
              try:
               event_url = data["_embedded"]["events"][x]["url"]
-              
              except KeyError:
                 event_url = "None"
 
              list_of_events = [event_name,genre_name,segment_name,address,city_name,localdate,localtime,event_url]  
-         
 
              x += 1
              info.append(list_of_events)
@@ -79,13 +70,10 @@ def fetch_events():
 
      return info      
           
-import csv
-
 def convert_to_csv(info):
-   file = open("events.csv", "w")
+   file = open("miniproject/events.csv", "w")
    writer = csv.writer(file)
-   #info = fetch_events()
-   headers = ["Event Name","Genre","Segment","Address","City","Local Date","Local Time","URL"]
+   headers = ["event_name","genre","segment","address","city","local_date","local_time","url"]
    info2 = [headers] + info
    for row in info2:
       print(row)
