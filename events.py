8b99ec36156de90f6c2928c9bf50583117d8861b
ilknurorman <91733625+ilknur11@users.noreply.github.com>
2024-02-21 15:43:57 +0300

    Add files via upload

---
+++ events.py
@@ -0,0 +1,102 @@
+import requests
+
+def fetch_events(city):
+    
+    url = "https://app.ticketmaster.com/discovery/v2/events.json"
+    params = {
+        "countryCode": "TR",
+        "apikey": "RyH8X5Z5A3wa7UHDaCwNfBwfMzYhJ4rh",
+        "city": city
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        events = data["_embedded"]["events"]
+
+        formatted_events = []
+
+        for event in events:
+            try:
+                name = event["name"]
+            except KeyError:
+                name = "None"
+            
+            try:
+                genre = event["classifications"][0]["genre"]["name"]
+            except KeyError:
+                genre = "None"
+
+            try:
+                segment = event["classifications"][0]["segment"]["name"]
+            except KeyError:
+                segment = "None"
+
+            try:
+                address = event["_embedded"]["venues"][0]["address"]["line1"]
+            except KeyError:
+                address = "None"
+
+            try:
+                city = event["_embedded"]["venues"][0]["city"]["name"]
+            except KeyError:
+                city = "None"
+
+            try:
+                localdate = event["dates"]["start"]["localDate"]
+            except KeyError:
+                localdate = "None"
+
+            try:
+                localtime = event["dates"]["start"]["localTime"]
+            except KeyError:
+                localtime = "None"
+
+            try:
+                event_url = event["url"]
+            except KeyError:
+                event_url = "None"
+
+            event_info = [name,genre,segment,address,city,localdate,localtime,event_url]
+
+            formatted_events.append(event_info)
+
+        return formatted_events
+    
+    else:
+        print("Error:", response.status_code)
+        return []
+    
+
+#print(fetch_events("Ankara"))
+
+
+import csv
+
+def convert_to_csv(json_data, csv_path):
+    try:
+        with open(csv_path, mode='w', newline='', encoding='utf-8') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=headers)
+
+            
+            headers = ['event_name', 'genre', 'segment', 'address', 'city', 'localdate', 'localtime', 'url']
+            writer.writeheader()
+
+            
+            for event in json_data:
+                
+                if not isinstance(event, dict):
+                    continue
+
+                writer.writerow(event)
+
+        print(f"CSV dosyası başarıyla oluşturuldu: {csv_path}")
+
+    except Exception as e:
+        print("CSV'ye dönüştürme hatası:", e)
+
+
+
+#events = fetch_events("Istanbul") 
+
