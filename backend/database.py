ac4183325243f612f4af72406e3f4c9b82a27622
gulsumsaluvan <gulsumsaluvan5@gmail.com>
2024-03-02 12:30:45 +0300

    02.03.2024

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, TIMESTAMP, Date
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.sql import func
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:150716@localhost/gs'
 
@@ -15,3 +16,15 @@ def get_db():
         yield db
     finally:
         db.close()
+
+class Post(Base):
+    __tablename__ = "events_table"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    event_name = Column(String, nullable=False)
+    genre = Column(String, nullable = False)
+    segment = Column(String, nullable = False)
+    address = Column(String, nullable = False)
+    localdate = Column(Date)
+    local_time = Column(TIMESTAMP(timezone=False), server_default=func.now())
+    url = Column(String, nullable = False)
--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,21 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+from database import Post, Base, engine
+from datetime import date, datetime
+
+class PostRequest(BaseModel):
+    event_name: str
+    genre: str
+    segment: str
+    address: str
+    localdate: date
+    local_time: datetime
+    url: str
 
 app = FastAPI()
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
 
 @app.put("/item/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_price": item.price, "item_id": item_id}
+def update_item(item_id: int, item: PostRequest):
+    return {"item_id": item.id, "item_name": item.event_name}
