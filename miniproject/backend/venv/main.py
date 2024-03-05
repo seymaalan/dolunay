2ef7b7580ecd9309fe622d2bc4084c8537f6569a
elifkirer <elifkirer1@gmail.com>
2024-03-05 23:26:02 +0300

    upcoming,past events

--- miniproject/backend/venv/main.py
+++ miniproject/backend/venv/main.py
@@ -1,22 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 import sys
 sys.path.append('/Users/elif/Desktop/dolunay/miniproject/backend/venv')
-from database import engine
-import models
+from database import engine,get_db
+import models,schemas
 from router import posts
+from sqlalchemy.orm import Session
+from typing import List
+from datetime import datetime
+
 
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
 models.Base.metadata.create_all(bind=engine)     
 
 app.include_router(posts.router)
+
+@app.get("/upcoming_events", response_model=List[schemas.CreatePost])
+def get_upcoming_events(db: Session = Depends(get_db)):
+    current_date = datetime.now()
+    upcoming_events = db.query(models.Events).filter(models.Events.local_date >= current_date).all()
+
+    return upcoming_events
+
+@app.get("/past_events", response_model=List[schemas.CreatePost])
+def get_past_events(db: Session = Depends(get_db)):
+    current_date = datetime.now()
+    past_events = db.query(models.Events).filter(models.Events.local_date < current_date).all()
+
+    return past_events
